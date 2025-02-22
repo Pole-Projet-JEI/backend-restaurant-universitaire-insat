75ba1f2032aba628dce0d7dd053d6b28dd84e429
Kousay Jebir <kousayjebir@gmail.com>
2025-02-22 11:41:07 +0100

    fix .env file not recognized in nest app

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { AppService } from './app.service';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: ".env.development"
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
