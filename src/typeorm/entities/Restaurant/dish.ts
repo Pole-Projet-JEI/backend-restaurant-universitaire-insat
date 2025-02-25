1fbc79f154305e37f1923e4fb2a73874dfdb10fa
Ahmed Idani <159375940+HIJOdelIDANII@users.noreply.github.com>
2025-02-25 19:40:42 +0100

    Develop data model (#4)
    * user entities
    
    * command entity and timestamp added
    
    * entities addeded : qrCode and wallet
    
    * added entities: ticket , transfer , restaurant entites
    
    * changed command to order
    
    * changed major attribute type in Student entity to be an enum
    
    * small change

---
+++ src/typeorm/entities/Restaurant/dish.ts
@@ -0,0 +1,25 @@
+import { IsInt, IsString } from 'class-validator';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { DayMenu } from './DayMenu';
+
+@Entity()
+export class Dish {
+  @PrimaryGeneratedColumn()
+  @IsInt()
+  id: number;
+
+  @Column()
+  @IsString()
+  name: string;
+
+  @Column()
+  @IsString()
+  description: string;
+
+  @Column()
+  @IsInt()
+  note: number;
+
+  @ManyToOne(() => DayMenu, (dayMenu) => dayMenu.dishes)
+  dayMenu: DayMenu;
+}
---
+++ src/typeorm/entities/Users/User.ts
@@ -0,0 +1,36 @@
+import { PrimaryColumn, Column } from 'typeorm';
+import { IsInt, IsString, IsEmail, Length, IsEnum } from 'class-validator';
+import { TimeStamp } from '../timeStamp';
+
+export enum UserRole {
+  STUDENT = 'student',
+  ADMIN = 'admin',
+  SUPERADMIN = 'superadmin',
+}
+
+export abstract class User extends TimeStamp {
+  @PrimaryColumn({ unique: true })
+  @IsInt()
+  nationalId: number;
+
+  @Column()
+  @IsString()
+  firstName: string;
+
+  @Column()
+  @IsString()
+  lastName: string;
+
+  @Column()
+  @IsEmail()
+  email: string;
+
+  @Column()
+  @IsString()
+  @Length(6, 250)
+  passwordHash: string;
+
+  @Column({ type: 'enum', enum: UserRole, default: UserRole.STUDENT })
+  @IsEnum(UserRole)
+  role: UserRole;
+}
